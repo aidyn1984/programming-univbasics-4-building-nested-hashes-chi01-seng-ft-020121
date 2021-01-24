5acbea9504ca8ae7e99bed50fb43a12b78a30eda
Val_1 S_z <csa.aiden@gmail.com>
2021-01-24 05:31:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,105 +1,75 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def base_hash
+  # Remember implicit return! We're returning this new Hash _without_ the
+  # keyword "return." Nice and neat.
+  {}
+  monopoly = {:railroads => {}}
 end
-introduction("Steven")
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def monopoly_with_second_tier
+  monopoly =  {
+     :railroads=>
+    {
+      :pieces=> 4,
+      :rent_in_dollars=> {},
+      :names=> {}
+     }
+ }
+  end
+
+def monopoly_with_third_tier
+  monopoly =  {
+    :railroads=>
+    {
+      :pieces=> 4,
+      :rent_in_dollars=>
+        {
+          :one_piece_owned=>25,
+          :two_pieces_owned=>50,
+          :three_pieces_owned=>100,
+          :four_pieces_owned=>200
+        },
+      :names=>
+        {
+          :reading_railroad => {},
+          :pennsylvania_railroad => {},
+          :b_and_o_railroad => {},
+          :shortline_railroad => {}
+        }
+    }
+  }
 end
-introduction_with_language("Maria", "Ruby")
 
-
-
-def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def monopoly_with_fourth_tier
+  monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
 end
-introduction_with_language_optional("Elena")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
-#end
-
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
-
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
